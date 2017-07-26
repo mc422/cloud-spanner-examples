9d678c9a3d4cab02ad9a84815430c7450512b31a
Mike Curtiss <curtiss@google.com>
2017-07-25 20:28:37 -0700

    cleanup

--- bank_example.py
+++ bank_example.py
@@ -9,15 +9,6 @@ import datetime
 import pprint
 
 
-def simple_query(database):
-    # Execute a simple SQL statement.
-    results = database.execute_sql('SELECT 7')
-
-    for row in results:
-        print(row)
-    # [END spanner_quickstart]
-
-
 def setup_accounts(database):
     """Inserts sample data into the given database.
 
@@ -38,13 +29,13 @@ def setup_accounts(database):
         batch.insert_or_update(
             table='Accounts',
             columns=('CustomerNumber', 'AccountNumber', 'AccountType',
-                     'Balance', 'CreationTime'),
+                     'Balance', 'CreationTime', 'LastInterestCalculation'),
             values=[
-                (1, 1, 0, 0, datetime.datetime.utcnow()),
-                (1, 2, 1, 0, datetime.datetime.utcnow()),
-                (2, 3, 0, 0, datetime.datetime.utcnow()),
-                (3, 4, 1, 0, datetime.datetime.utcnow()),
-                (4, 5, 0, 0, datetime.datetime.utcnow()),
+                (1, 1, 0, 0, datetime.datetime.utcnow(), None),
+                (1, 2, 1, 0, datetime.datetime.utcnow(), None),
+                (2, 3, 0, 0, datetime.datetime.utcnow(), None),
+                (3, 4, 1, 0, datetime.datetime.utcnow(), None),
+                (4, 5, 0, 0, datetime.datetime.utcnow(), None),
                  ])
 
         batch.delete(
@@ -88,12 +79,8 @@ def extract_single_cell(results):
 
 
 def account_balance(database, account_number):
-    params = {
-        'account': account_number,
-    }
-    param_types = {
-        'account': type_pb2.Type(code=type_pb2.INT64),
-    }
+    params = {'account': account_number}
+    param_types = {'account': type_pb2.Type(code=type_pb2.INT64)}
     results = database.execute_sql(
         """SELECT Balance From Accounts@{FORCE_INDEX=UniqueAccountNumbers}
            WHERE AccountNumber=@account""",
@@ -104,12 +91,8 @@ def account_balance(database, account_number):
 
 
 def customer_balance(database, customer_number):
-    params = {
-        'customer': customer_number,
-    }
-    param_types = {
-        'customer': type_pb2.Type(code=type_pb2.INT64),
-    }
+    params = {'customer': customer_number}
+    param_types = {'customer': type_pb2.Type(code=type_pb2.INT64)}
     results = database.execute_sql(
         """SELECT SUM(Accounts.Balance) From Accounts INNER JOIN Customers
            ON Accounts.CustomerNumber=Customers.CustomerNumber
@@ -171,62 +154,76 @@ class RowAlreadyUpdated(Exception):
     pass
 
 
-def compute_interest_for_all(database):
-    def compute_interest(transaction, customer_number, account_number):
-        # re-check (within the transaction) that the account has not been
-        # updated for the current month
-        results = transaction.execute_sql(
-            """
-    SELECT Balance,CURRENT_TIMESTAMP() FROM Accounts
-    WHERE AccountNumber=@account AND   # ONLY fetch this one row!
+def compute_interest_for_account(transaction, customer_number, account_number,
+                                 last_interest_calculation):
+    # re-check (within the transaction) that the account has not been
+    # updated for the current month
+    results = transaction.execute_sql(
+        """
+    SELECT Balance, CURRENT_TIMESTAMP() FROM Accounts
+    # ONLY fetch this one row!
+    WHERE CustomerNumber=@customer AND AccountNumber=@account AND
           (LastInterestCalculation IS NULL OR
-          (EXTRACT(MONTH FROM LastInterestCalculation) <> EXTRACT(MONTH FROM CURRENT_TIMESTAMP()) AND
-          EXTRACT(YEAR FROM LastInterestCalculation) <> EXTRACT(YEAR FROM CURRENT_TIMESTAMP())))
-   LIMIT 1""",
-            params={'account': account_number},
-            param_types={'account': type_pb2.Type(code=type_pb2.INT64)})
+           LastInterestCalculation=@calculation)""",
+        params={'customer': customer_number,
+                'account': account_number,
+                'calculation': last_interest_calculation},
+        param_types={'customer': type_pb2.Type(code=type_pb2.INT64),
+                     'account': type_pb2.Type(code=type_pb2.INT64),
+                     'calculation': type_pb2.Type(code=type_pb2.TIMESTAMP)})
+    try:
+        old_balance, current_timestamp = extract_single_row_to_tuple(results)
+    except:
+        # An exception means that the row has already been updated.
+        # Abort the transaction.
+        raise RowAlreadyUpdated
 
-        try:
-            old_balance, current_timestamp = extract_single_row_to_tuple(results)
-        except:
-            # An exception means that the row has already been updated.
-            # Abort the transaction.
-            raise RowAlreadyUpdated
-        cents = int(0.01 * old_balance)  # monthly interest 1%
-        new_balance = old_balance + cents
-        transaction.update(
-            table='Accounts',
-            columns=('CustomerNumber', 'AccountNumber', 'Balance', 'LastInterestCalculation'),
-            values=[
-                (customer_number, account_number, new_balance, current_timestamp),
-                 ])
+    # Ignoring edge-cases around new accounts and pro-rating first month
+    cents = int(0.01 * old_balance)  # monthly interest 1%
+    new_balance = old_balance + cents
+    transaction.update(
+        table='Accounts',
+        columns=('CustomerNumber', 'AccountNumber', 'Balance',
+                 'LastInterestCalculation'),
+        values=[
+            (customer_number, account_number, new_balance, current_timestamp),
+            ])
 
-        transaction.insert(
-            table='AccountHistory',
-            columns=('AccountNumber', 'Ts', 'ChangeAmount', 'Memo'),
-            values=[
-                (account_number, current_timestamp, cents, 'Monthly Interest'),
-                ])
+    transaction.insert(
+        table='AccountHistory',
+        columns=('AccountNumber', 'Ts', 'ChangeAmount', 'Memo'),
+        values=[
+            (account_number, current_timestamp, cents, 'Monthly Interest'),
+            ])
 
+
+def compute_interest_for_all(database):
     while True:
         # Find any account that hasn't been updated for the current month
         # (This is done in a read-only transaction, and hence does not
         # take locks on the table)
+        # Note: In a real production DB, we would process rows in batches
+        # of N instead of batches of 1.
         results = database.execute_sql(
             """
-    SELECT CustomerNumber,AccountNumber FROM Accounts
+    SELECT CustomerNumber,AccountNumber,LastInterestCalculation FROM Accounts
     WHERE LastInterestCalculation IS NULL OR
-    (EXTRACT(MONTH FROM LastInterestCalculation) <> EXTRACT(MONTH FROM CURRENT_TIMESTAMP()) AND
-    EXTRACT(YEAR FROM LastInterestCalculation) <> EXTRACT(YEAR FROM CURRENT_TIMESTAMP()))
+    (EXTRACT(MONTH FROM LastInterestCalculation) <>
+       EXTRACT(MONTH FROM CURRENT_TIMESTAMP()) AND
+     EXTRACT(YEAR FROM LastInterestCalculation) <>
+       EXTRACT(YEAR FROM CURRENT_TIMESTAMP()))
     LIMIT 1""")
         try:
-            customer_number, account_number = \
+            customer_number, account_number, last_interest_calculation = \
                 extract_single_row_to_tuple(results)
         except:
+            # results were empty. No more rows to process.
             break
         try:
-            database.run_in_transaction(compute_interest,
-                                        customer_number, account_number)
+            database.run_in_transaction(compute_interest_for_account,
+                                        customer_number, account_number,
+                                        last_interest_calculation)
+            print "Computed interest for account ", account_number
         except RowAlreadyUpdated:
             print "Caught RowAlreadyUpdated"
             pass
@@ -248,18 +245,18 @@ def main():
     # Get a Cloud Spanner database by ID.
     database = instance.database(database_id)
 
-    simple_query(database)
     setup_accounts(database)
-
-
     account_balance(database, 2)
     customer_balance(database, 1)
     deposit(database, 1, 1, 150, 'Dollar Fifty Deposit')
     deposit(database, 1, 2, 75)
+    for i in range(20):
+        deposit(database, 3, 4, i * 100, 'Deposit %d dollars' % i)
     account_balance(database, 2)
     customer_balance(database, 1)
-    last_n_transactions(database, 1, 2)
+    last_n_transactions(database, 4, 10)
     compute_interest_for_all(database)
+
 
 if __name__ == "__main__":
     main()
